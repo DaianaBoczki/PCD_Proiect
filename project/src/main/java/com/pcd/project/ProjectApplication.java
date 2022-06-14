25d39c1ed538ad5e8e0957e39e5004cec5307059
DaianaBoczki <daiana.boczki00@e-uvt.ro>
2022-06-14 20:41:46 +0300

    rest client update

--- project/src/main/java/com/pcd/project/ProjectApplication.java
+++ project/src/main/java/com/pcd/project/ProjectApplication.java
@@ -26,30 +26,34 @@ public class ProjectApplication {
 		HttpEntity<String> entity = new HttpEntity<>(data, headers);
 		//restTemplate.postForObject(path, entity, String.class);
 		//String response = restTemplate.postForObject(path, entity, String.class);
-		//String response = restTemplate.exchange(path, HttpMethod.POST, entity, String.class).getBody();
-		//System.out.println(response);
+		String response = restTemplate.exchange(path, HttpMethod.POST, entity, String.class).getBody();
+		System.out.flush();
+		System.out.println(response);
 
-		System.out.println(restTemplate.exchange(path, HttpMethod.POST, entity, char.class).getBody());
-		return null;
+//		System.out.println((restTemplate.exchange(path, HttpMethod.POST, entity, String.class).getBody())+ "\n");
+//		System.out.flush();
+		return response;
 	}
 
-	public static void main(String[] args) throws IOException {
-		//clientHttpRequestFactory.setReadTimeout(3000);
-		restTemplate = new RestTemplate(clientHttpRequestFactory);
+	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+		//SpringApplication.run(ProjectApplication.class, args);
+
+		//clientHttpRequestFactory.setReadTimeout(10000);
+		//restTemplate = new RestTemplate(clientHttpRequestFactory);
+		restTemplate = new RestTemplate();
 //		List<HttpMessageConverter<?>> messageConverters = new ArrayList<HttpMessageConverter<?>>();
 //		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-//		converter.setSupportedMediaTypes(Collections.singletonList(MediaType.ALL));
+//		converter.setSupportedMediaTypes(Collections.singletonList(MediaType.TEXT_PLAIN));
 //		messageConverters.add(converter);
+//
 //		restTemplate.setMessageConverters(messageConverters);
-		SpringApplication.run(ProjectApplication.class, args);
 		HttpHeaders headers = new HttpHeaders();
-		//headers.setAccept(Arrays.asList(MediaType.TEXT_PLAIN));
 		headers.setAccept(Arrays.asList(MediaType.ALL));
 
 		Scanner input = new Scanner(System.in);
 		int opt, cont = 0, id, i, j, k, l, songSize, requestId, octSize, packetsNo, actualSongSize, toSendSize, dataSize = 1024;
 		File audio;
-		String data, songTitle, songGenre, sentNameGenre, auxSize, auxRequestId, auxOct, songbytes, path, sentAudio;
+		String data, songTitle, songGenre, sentNameGenre, auxSize = "", auxRequestId = "", auxOct = "", songbytes, path, sentAudio;
 
 		while (cont == 0) {
 			System.out.println("\nChoose an option:\n1 - Listen to a song\n2 - Check uploads\n3 - Upload file\n");
@@ -59,13 +63,51 @@ public class ProjectApplication {
 					actualSongSize = 0;
 					System.out.println("\nThe ID of the wanted song: ");
 					id = input.nextInt();
+					songTitle = String.format("song%d.mp3", id);
+					data = String.format("---1---%d", id);
+					data = sendRequestType(headers, data);
+
+					j = 0;
+					for(i = 4; i < 8; i++){
+						if(Character.isDigit(data.charAt(i))) {
+							auxSize.concat(String.valueOf(data.charAt(i)));
+							j++;
+						}
+					}
+					songSize = Integer.valueOf(auxSize);
+
+					j = 0;
+					for(i = 8; i < 12; i++){
+						if(Character.isDigit(data.charAt(i))) {
+							auxRequestId.concat(String.valueOf(data.charAt(i)));
+							j++;
+						}
+					}
+					requestId = Integer.valueOf(auxRequestId);
+
+					data = String.format("---%d---%d---%d", 6, requestId, requestId);
+					data = sendRequestType(headers, data);
+
+					l = 0;
+					PrintWriter writer = new PrintWriter(songTitle);
+					for(i = 0; i < songSize; i++){
+						j = 0;
+						for(k = 4; k < 8; k++){
+							auxOct.concat(String.valueOf(data.charAt(k)));
+							j++;
+						}
+						octSize = Integer.valueOf(auxOct);
+						writer.print(data);
+						data = String.format("---%d---%d---%d", 6, requestId, requestId);
+						data = sendRequestType(headers, data);
+					}
 
 					break;
 
 				case 2:
 					data = String.format("---%d", 2);
 					data = sendRequestType(headers, data);
-					System.out.println(data);
+					//System.out.println(data);
 
 					//requestId = Integer.valueOf(data.charAt(11));
 					data = String.format("---%d---%d---%d", 6, 0, 0);
